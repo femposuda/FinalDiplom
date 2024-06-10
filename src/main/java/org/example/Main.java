fa4e797b7f5f41d4a598c5c52d8ca76c79192d6d
Maria <mariarayasan@gmail.com>
2024-06-10 18:14:15 +0300

    change name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Account account = new Account("Иван Иваныч");
+        Account account = new Account("Иван Иван");
         System.out.println(account.checkNameString());
     }
 }
